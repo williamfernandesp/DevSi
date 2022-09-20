f6f25d0e43d996bcccb77df901ed2a28fcdc9e44
William Fernandes Pereira <williamfernandesp@outlook.com>
2022-09-20 17:06:20 -0300

    modificando

--- contato.php
+++ contato.php
@@ -1,6 +1,48 @@
 <?php include "./cabecalho.php"; ?>
-
+<?php
+   if (!empty($_POST) && isset($_POST)){
+      if (empty($_POST["email"])) {
+         ?>
+            <div class="alert alert-danger">
+               O Campo email deve ser preenchido
+            </div>
+         <?php
+      } else if(empty($_POST["nome"])){
+         ?>
+         <div class="alert alert danger">
+            O campo nome deve ser preenchido
+         </div>
+         <?php
+      }
+      else{
+         ?>
+            <div class="alert alert-success">
+               Sua Reclamação foi enviada com sucesso!
+            </div>
+         <?php
+      }
+   }
+?>
    <h1>Entre em contato conosco</h1>
 
-   
+<form action="./contato.php" method="post">
+
+   <div class="form-group col-4 offset-4">
+      <label>Nome</label>
+      <input type="text" name="nome" class="form-control">
+   </div>   
+
+   <div class="form-group col-4 offset-4">
+      <label>Email</label>
+      <input type="email" require name="email" class="form-control">
+   </div>
+   <br>
+   <div class="form-group col-4 offset-4">
+   <textarea class="form-control" name="obs"></textarea>
+   </div>
+   <br>
+   <button type="submit" class="btn btn-warning">
+      Enviar dados
+   </button>
+</form>
 <?php include "./rodape.php"; ?>
--- produtos.php
+++ produtos.php
@@ -1,11 +1,69 @@
 <?php include "./cabecalho.php"; ?>
-<h1>Minha lista de produtos</h1>
-$_GET
-<pre>
-<?php 
-    print_r($_GET);
-?>
-</pre>
 
+<h1>Minha lista de produtos</h1>
+
+<form action="./produtos.php" method="post">
+
+   <div class="form-group col-4 offset-4">
+      <label>Descrição</label>
+      <input type="text" name="descricao" class="form-control">
+   </div>
+
+   <div class="form-group col-4 offset-4">
+      <label>Valor</label>
+      <input type="text" name="valor" class="form-control">
+   </div>
+   <br>
+   <div class="form-group col-4 offset-4">
+      <label>Quantidade em Estoque</label>
+      <input type="text" name="qtdEstoque" class="form-control">
+   </div>
+   <br>
+   <button type="submit" class="btn btn-warning">
+      Enviar dados
+   </button>
+</form>
+
+<?php
+   if (!empty($_POST) && isset($_POST)){
+      if (empty($_POST["descricao"])) {
+         ?>
+            <div class="alert alert-danger">
+               O Campo Descrição deve ser preenchido
+            </div>
+         <?php
+      } else if(empty($_POST["valor"])){
+         ?>
+         <div class="alert alert danger">
+            O campo nome1 deve ser preenchido
+         </div>
+         <?php
+      } else if(empty($_POST["qtdEstoque"])){
+        ?>
+        <div class="alert alert danger">
+           O campo Quantidade de Estoque deve ser preenchido
+        </div>
+        <?php
+      }else{
+         ?>
+            <table class="table">
+                <tr>
+                    <td>
+                        <?php print_r($_POST["descricao"])?>
+                    </td>
+                    
+                    <td>
+                        <?php print_r($_POST["valor"])?>
+                    </td>
+                    
+                    <td>
+                        <?php print_r($_POST["qtdEstoque"])?>
+                    </td>
+                </tr>
+            </table>
+         <?php
+      }
+   }
+?>
 
 <?php include "./rodape.php"; ?>
